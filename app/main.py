8bf1b8de4f9058d5773982e4b6ad6458abee586c
heiimics <salilimicaella@gmail.com>
2026-07-12 23:46:19 +0800

    Refactor: Move backend to root and configure for Vercel deployment

--- app/main.py
+++ app/main.py
@@ -21,7 +21,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://axis-eight-navy.vercel.app/" # <--- ADD YOUR VERCEL URL HERE
     ],
     allow_credentials=True,
     allow_methods=["*"],
